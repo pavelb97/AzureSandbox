c003dd7c669c74aeb2d0d9eb3474831e636c156e
pavelb97 <pavel.belitskiy@gmail.com>
2023-06-30 11:16:26 +0100

    Adds base FA components: Resource group, storage account, and app service plan.

--- main.tf
+++ main.tf
@@ -9,4 +9,33 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id            = "a4e59050-bf58-4a23-941a-974fafd931ad"
+  skip_provider_registration = true     # Prevents School & Personal credential clashing
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "${var.project}-resource-group"
+  location = var.location
+}
+
+# FAs require a storage account to host the app container
+resource "azurerm_storage_account" "storage_account" {
+  name = "${var.project}-storage"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location = var.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"      # Local Redundant Storage
+}
+
+# FAs require a ASP - used to define the scope of resources associated with the FA
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "${var.project}-app-service-plan"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = var.location
+  kind                = "FunctionApp"
+  reserved = true                       # Must be set to true for linux
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
 }
